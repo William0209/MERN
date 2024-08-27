35c782d8ed77a663906ccd41e75ae3f4c5c77060
William0209 <janssenwilliam02@gmail.com>
2024-08-27 21:34:41 +0200

    DB connection

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 // middleware
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // database connection
-const dbURI = 'mongodb+srv://shaun:test1234@cluster0.del96.mongodb.net/node-auth';
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then((result) => app.listen(3000))
+const dbURI =
+  "mongodb+srv://janssenwilliam02:Tildewille00@smoothie.5k26p.mongodb.net/node-auth";
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((result) => {
+    console.log(
+      "Connected to the database"
+    );
+    app.listen(3000, () => {
+      console.log(
+        "Server is running on port 3000"
+      );
+    });
+  })
   .catch((err) => console.log(err));
 
 // routes
-app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get("/", (req, res) =>
+  res.render("home")
+);
+app.get("/smoothies", (req, res) =>
+  res.render("smoothies")
+);
