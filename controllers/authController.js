443ac5d012a26c3379292c8541ecf4d5a0a5a67d
William0209 <janssenwilliam02@gmail.com>
2024-08-28 16:41:55 +0200

    chore: Update dependencies and implement cookie parsing

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 
 // handle errors
 const handleErrors = (err) => {
@@ -20,6 +21,14 @@ const handleErrors = (err) => {
   return errors;
 };
 
+// create json web token
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+  return jwt.sign({ id }, "wj super secret secret", {
+    expiresIn: maxAge,
+  });
+};
+
 // controller actions
 
 module.exports.signup_get = (req, res) => {
@@ -35,7 +44,9 @@ module.exports.signup_post = async (req, res) => {
 
   try {
     const user = await User.create({ email, password });
-    res.status(201).json(user);
+    const token = createToken(user._id);
+    res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(201).json({ user: user._id });
   } catch (err) {
     const errors = handleErrors(err);
     res.status(400).json({ errors });
